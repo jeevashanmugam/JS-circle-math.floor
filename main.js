e3457f0f07b5cf5098450b13df0a7c445be3656f
jeevashanmugam <jeevasj2000@gmail.com>
2023-07-11 13:51:52 +0530

    JS circle math.floor

---
+++ main.js
@@ -0,0 +1,18 @@
+
+// Using Map
+
+let circles = [10, 30, 50];
+
+let value = circles.map(function (e) {
+
+    pi = 3.141592
+
+    ans = Math.floor(pi * (e ** 2))
+
+    return ans
+})
+
+document.write(`Round value of : ${value}`)
+
+
+
